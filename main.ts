bd57bdcc6bce0d9012ddcb1da689e05c7dd82ddf
imuradevelopment <imuradevelopmentauth@gmail.com>
2020-11-20 23:03:27 +0900

    TypeScript学習用プロジェクト作成

---
+++ main.ts
@@ -0,0 +1,11 @@
+import { Logger } from './log';
+
+Logger.log('Typescript works!')
+
+
+// 2次元配列（配列の配列）
+const matrix: string[] = [
+    ["a", "b"],
+    ["c", "d"]
+];
+console.log(matrix[0][0]); // => "a"
